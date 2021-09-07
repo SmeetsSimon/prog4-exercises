1375c613b66daedbecb6ae0d45799a0f18c469e4
Panagiotis H.M. Issaris <takis@issaris.com>
2021-09-07 07:21:29 +0200

    Add exercises on lists

---
+++ src/part3.py
@@ -0,0 +1,50 @@
+# Your exercises should appear in this file.
+
+
+def list_search(l, value):
+    """Return the index of the value in the list"""
+    return 0
+
+
+def phonebook_search(phonebook, search_name):
+    """Return het telefoonnummer van de gevraagde persoon
+
+    Als de persoon niet in het telefoonboek staat, geef dan None terug.
+
+    phonebook: een list van lists met hierin naam, telefoonnummer paren
+    search_name: de naam van de te zoeken persoon
+    """
+    return 0
+
+
+def phonebook_add(phonebook, name, number):
+    """Voeg het telefoonnummer voor 'name' aan de lijst toe.
+
+    Als het koppel name,number al in het telefoonboek zit,
+    voeg het dan niet toe, maar toon "data reeds in telefoonboek"
+    op het scherm.
+    Als de telefoonnummer reeds voor een ander persoon in het telefoonboek
+    staat, voeg het dan niet toe, maar toon "ander persoon met deze nummer
+    in telefoonboek" op het scherm.
+    """
+    print("not implemented")
+
+
+def phonebook_remove(phonebook, name, number):
+    """Verwijder het koppel name,number van het telefoonboek.
+
+    Indien het koppel niet voorkomt, print dan
+    "persoon niet gevonden in telefoonboek" op het scherm.
+    """
+    print("not implemented")
+
+
+def phonebook_print(phonebook, search_name):
+    """Print de telefoonnummers van de gevraagde persoon
+
+    Als de persoon niet in het telefoonboek staat, toon dan "niets gevonden".
+
+    phonebook: een list van lists met hierin naam, telefoonnummer paren
+    search_name: de naam van de te zoeken persoon
+    """
+    print("not implemented")
