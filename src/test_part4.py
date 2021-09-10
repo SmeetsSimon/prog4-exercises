e76cf446ffa8a2e036f34d9fd80ce70904582b91
Takis Issaris <takis.issaris@mosa-rt.be>
2021-09-10 11:00:59 +0200

    Add new exercises

--- src/test_part4.py
+++ src/test_part4.py
@@ -19,3 +19,27 @@ def test_addressbook_search_not_found():
         {"name": "Klaas Gadeyne", "address": "Groenstraat 44"},
     ]
     assert addressbook_search(addressbook, "Jan Leenders") == None
+
+
+def test_addressbook_add():
+    addressbook = [
+        {"name": "Piet Dirkx", "address": "Kerkstraat 32"},
+        {"name": "Klaas Gadeyne", "address": "Groenstraat 44"},
+    ]
+    addressbook_add(addressbook, "Jan Peters", "Dorpsstraat 11")
+    assert len(addressbook) == 3
+    assert {"name": "Jan Peters", "address": "Dorpsstraat 11"} in addressbook
+
+
+def test_addressbook_add_duplicate(capsys):
+    addressbook = [
+        {"name": "Jan Peters", "address": "Dorpsstraat 11"},
+        {"name": "Piet Dirkx", "address": "Kerkstraat 32"},
+        {"name": "Klaas Gadeyne", "address": "Groenstraat 44"},
+    ]
+    addressbook_add(addressbook, "Jan Peters", "Dorpsstraat 11")
+    assert len(addressbook) == 3
+    assert {"name": "Jan Peters", "address": "Dorpsstraat 11"} in addressbook
+
+    captured = capsys.readouterr()
+    assert captured.out == "persoon reeds in adresboek\n"
