f26601a094d58914949595f72d2519c92572829a
SmeetsSimon <simonsmeets99@gmail.com>
2022-03-22 14:40:01 +0100

    g

--- src/t1_extra_dict.py
+++ src/t1_extra_dict.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def maak_persoonsinformatie_dict(naam, leeftijd, gewicht, lengte, oogkleur):
     """Geef een dictionary terug met alle gegevens die als parameters aan
     de functie meegegeven werden.
@@ -6,6 +8,15 @@ def maak_persoonsinformatie_dict(naam, leeftijd, gewicht, lengte, oogkleur):
     >>> maak_persoonsinformatie_dict("Jan", 32, 79, 167, "blauw")
     {'naam': 'Jan', 'leeftijd': 32, 'gewicht': 79, 'lengte': 167, 'oogkleur': 'blauw'}
     """
+    geg = {
+        "naam" : naam,
+        "leeftijd" : leeftijd, 
+        "gewicht" : gewicht,
+        "lengte" : lengte, 
+        "oogkleur" : oogkleur 
+
+    }
+    return geg
 
 
 def tel_autos(lijst_autos):
@@ -16,6 +27,34 @@ def tel_autos(lijst_autos):
     >>> tel_autos(["bmw", "audi", "audi", "ford", "bmw"])
     {'peugeot': 0, 'ford': 1, 'bmw': 2, 'audi': 2, 'nissan': 0}
     """
+    lijst_peugeot = 0
+    lijst_ford = 0
+    lijst_bmw = 0
+    lijst_audi = 0
+    lijst_nissan = 0
+
+    for i in lijst_autos:
+        if "peugeot"in i["merk"]:
+            lijst_peugeot += 1
+        if "ford"in i["merk"]:
+            lijst_ford += 1
+        if "bmw"in i["merk"]:
+            lijst_bmw += 1
+        if "audi"in i["merk"]:
+            lijst_audi += 1
+        if "nissan"in i["merk"]:
+            lijst_nissan += 1   
+
+    dict = {
+        "peugeot" : lijst_peugeot,
+        "ford" : lijst_ford,
+        "bmw" : lijst_bmw,
+        "audi" : lijst_audi,
+        "nissan" : lijst_nissan,
+    }  
+
+    return dict  
+
 
 
 def leeftijden_acteurs(acteurs):
@@ -26,6 +65,10 @@ def leeftijden_acteurs(acteurs):
     >>> leeftijden_acteurs([["Will Smith", 53], ["Tom Hanks", 65]])
     [53, 65]
     """
+    lijst = []
+    for actuer in acteurs:
+        lijst.append(actuer[1])
+    return lijst
 
 
 def lengtes_acteurs(acteurs):
@@ -36,3 +79,7 @@ def lengtes_acteurs(acteurs):
     >>> lengtes_acteurs([{"naam": "Jennifer Lawrence", "leeftijd": 31, "lengte": 175}])
     [175]
     """
+    lijst = []
+    for acteur in acteurs:
+        lijst.append(acteur["lengte"])
+    return lijst
